280e8bb20394200bcb68c5bc6efac6c7664f9a0d
Yadnik Bendale <66904635+Yadnik1@users.noreply.github.com>
2024-04-27 18:37:29 -0400

    Update mcp23017_samw25.h

--- i2c_imu/Application/src/mcp23017_samw25.h
+++ i2c_imu/Application/src/mcp23017_samw25.h
@@ -4,20 +4,21 @@
 #include "I2cDriver.h"
 #include "SerialConsole.h"
 
-// MCP23017 registers
-#define MCP23017_IODIRA 0x00
-#define MCP23017_GPPUA 0x0C
-#define MCP23017_GPIOA 0x12
-#define MCP23017_ADDRESS 0x20 // Address with A2, A1, A0 to GND
+// Register addresses for MCP23017
+#define MCP23017_IODIRA 0x00  // I/O direction register for port A; 1=input, 0=output
+#define MCP23017_GPPUA 0x0C   // GPIO pull-up resistor register for port A
+#define MCP23017_GPIOA 0x12   // GPIO register for port A to read the pins state
+#define MCP23017_ADDRESS 0x20 // Default I2C address with A2, A1, A0 set to GND
 
-// Button defines
-#define SPI_BUTTON 1   // GPA0
-#define I2C_BUTTON 3   // GPA1
-#define UART_BUTTON 2  // GPA2
-#define GPIO_BUTTON 0  // GPA0
+// Definitions for button pin mapping on GPIOA
+#define SPI_BUTTON 1   // Bit position for SPI button (GPA1)
+#define I2C_BUTTON 3   // Bit position for I2C button (GPA3)
+#define UART_BUTTON 2  // Bit position for UART button (GPA2)
+#define GPIO_BUTTON 0  // Bit position for GPIO button (GPA0)
 
-void mcp23017_init(void);
-uint8_t mcp23017_read_gpio(void);
-void mcp23017_print_button(uint8_t gpio_state);
+// Function prototypes
+void mcp23017_init(void);                   // Initialize MCP23017 settings
+uint8_t mcp23017_read_gpio(void);           // Read current state of GPIOA pins
+void mcp23017_print_button(uint8_t gpio_state); // Print which button is pressed
 
 #endif /* MCP23017_H_ */
